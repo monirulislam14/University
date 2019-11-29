0f59200be18f53b745a5f3187e20b2c8c15451d7
monirulislam14 <damfast14@gmail.com>
2019-11-29 19:30:26 +0600

    start project

---
+++ UniversityCourseAndResultManagement/IRepository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using UniversityCourseAndResultManagement.DAL;
+
+namespace UniversityCourseAndResultManagement.IRepository
+{
+    public class GenericRepository<Tbl_Entity>:IRepository<Tbl_Entity> where Tbl_Entity : class
+    {
+        DbSet<Tbl_Entity> _dbSet;
+        private UniversityManagementDBEntities _DBEntity;
+
+        public GenericRepository(UniversityManagementDBEntities DBEntity)
+        {
+            _dbSet = DBEntity.Set<Tbl_Entity>();
+            _DBEntity = DBEntity;
+        }
+
+        public IEnumerable<Tbl_Entity> GetAllRecords()
+        {
+            return _dbSet.ToList();
+        }
+        public void Add(Tbl_Entity entity)
+        {
+            _dbSet.Add(entity);
+            _DBEntity.SaveChanges();
+        }
+        public void Update(Tbl_Entity entity)
+        {
+            _dbSet.Attach(entity);
+            _DBEntity.Entry(entity).State = EntityState.Modified;
+            _DBEntity.SaveChanges();
+
+        }
+    }
+}
